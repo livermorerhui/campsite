8668c0e0c8a648906107ecebb077ccfd221c2aba
xuhangwu <xuhangwu5244@gmail.com>
2017-01-29 21:43:18 +0800

    implement mission CRUD

---
+++ app/controllers/missions_controller.rb
@@ -0,0 +1,65 @@
+class MissionsController < ApplicationController
+  before_action :set_mission, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  
+  # GET /missions
+  # GET /missions.json
+  def index
+    @missions = Mission.all
+  end
+
+  # GET /missions/1
+  # GET /missions/1.json
+  def show
+  end
+
+  # GET /missions/new
+  def new
+    @mission = Mission.new
+  end
+
+  # POST /missions
+  # POST /missions.json
+  def create
+    @mission = Mission.new(mission_params)
+
+    if @mission.save
+      redirect_to missions_path, notice: 'Mission was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # GET /missions/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /missions/1
+  # PATCH/PUT /missions/1.json
+  def update
+    if @mission.update(mission_params)
+      redirect_to missions_path, notice: 'Mission was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /missions/1
+  # DELETE /missions/1.json
+  def destroy
+    @mission.destroy
+
+    redirect_to missions_path, notice: 'Mission was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_mission
+      @mission = Mission.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def mission_params
+      params.require(:mission).permit(:title, :description, :location)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170129101422) do
+ActiveRecord::Schema.define(version: 20170129122428) do
+
+  create_table "missions", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.text     "location"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
