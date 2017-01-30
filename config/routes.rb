d44bcf8a0535a12ce5a0cffae055842b130ae126
xuhangwu <xuhangwu5244@gmail.com>
2017-01-30 13:40:47 +0800

    add jobs nested route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'missions#index'
-  resources :missions
-  
+  resources :missions do
+    resources :jobs
+  end
+
   namespace :admin do
     resources :missions
+    resources :jobs
   end
 
 end
