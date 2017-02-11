aa2798a31f09bf24a6837e16e8c6c825a57fdfe6
xuhangwu <xuhangwu5244@gmail.com>
2017-02-11 10:47:27 +0800

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -33,8 +33,11 @@ Rails.application.routes.draw do
     resources :products
   end
 
-  resources :products
-
+  resources :products do
+    member do
+      post :add_to_cart
+    end
+  end
   ########        以下是根页面          ######################################################
 
   root 'welcome#index'
